1e549af827815716f7be85c198b7054bb7d2e683
Morsalin <morsalinkausar043@gmail.com>
2021-11-29 17:41:12 +0600

    done

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 const loading = document.getElementById("loading");
 const navigation = document.getElementById("navigation");
+const toggle_btn = document.getElementById("toggle_btn");
+const mobile_nav = document.getElementById("mobile_nav");
 
 // adding loading system ------------------>
 window.addEventListener("load", _ => loading.style.display = "none");
 
 // scroll system add ----------------------->
 window.addEventListener("scroll", _ => navigation.classList.toggle("active", window.scrollY > 0));
+
+// toggle_btn system add ----------------------->
+toggle_btn.addEventListener("click", _ => {
+    toggle_btn.classList.toggle("active");
+    mobile_nav.classList.toggle("active");
+});
 
 // type animation add ----------------------->
 
